637c470ea7625f3b9b77668f5d1a14b43718c178
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2026-03-08 23:38:36 -0300

    feat: implesments same thing another commit

--- factorial-recursive/main.js
+++ factorial-recursive/main.js
@@ -1,11 +1,29 @@
-function factorial(n){
-    let constante = 1
-    
-    for( i=n; i>=1; i--){
-        constante = constante * i
-    }
-    return constante
-}
-let fat = parseInt(prompt('Digite o fatorial desejado: '))
+const asciichart = require('asciichart')
 
-console.log(factorial(fat))
+function factorial(n) {
+    let constante = 1n
+    const dados = []
+    let inicio = Date.now()
+    const usedHeapSize = process.memoryUsage().heapUsed;
+
+    for (let i = n; i >= 1; i--) {
+        constante = constante * BigInt(i)
+        dados.push(constante.toString().length)
+    }
+
+    let fim = Date.now()
+    return { constante, dados, inicio, fim, usedHeapSize }
+}
+
+let fat = 10
+let resultado = factorial(fat)
+
+console.log("-------------------------------")
+console.log("Gráfico do crescimento do número de dígitos do fatorial:")
+console.log(asciichart.plot(resultado.dados))
+console.log("-------------------------------")
+console.log("O tempo de execução foi: " + (resultado.fim - resultado.inicio) / 1000 + " segundos")
+console.log("-------------------------------")
+console.log(`Memória heap JS usada: ${resultado.usedHeapSize / 1000000} MB`)
+console.log("-------------------------------")
+console.log("O fatorial de " + fat + " é: " + resultado.constante)
