aa80c455de6e68b6a1e8fec17a1e29352d9c0761
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2026-03-08 23:31:01 -0300

    feat: implements graphs and memory temp and execution time

--- factorial/main.js
+++ factorial/main.js
@@ -1,8 +1,22 @@
-let fat = parseInt(prompt('Digite o fatorial desejado: '))
-let constante = 1
+const asciichart = require('asciichart')
+let fat = 10
+let constante = 1n
+let dados = []
 
+let inicio = Date.now()
+const usedHeapSize = process.memoryUsage().heapUsed;
 for( i=fat; i>=1; i--){
-    constante = constante * i
+    constante = constante * BigInt(i)
+    dados.push(constante.toString().length)
 }
+let fim = Date.now()
 
-console.log(constante)
+console.log("-------------------------------")
+console.log("Gráfico do crescimento do número de dígitos do fatorial:")
+console.log(asciichart.plot(dados))
+console.log("-------------------------------")
+console.log("O tempo de execução foi: " + (fim - inicio) / 1000 + " segundos")
+console.log("-------------------------------")
+console.log(`Memória heap JS usada: ${usedHeapSize / 1000000} MB`);
+console.log("-------------------------------")
+console.log(`O fatorial de ${fat} é: ${constante}`)
